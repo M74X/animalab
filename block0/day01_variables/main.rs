e8d3cc66abaf1916228d5f5a778ac7c5f5df40cc
Hendrik Ordoñez Bullon <hendrikobuw@gmail.com>
2026-03-19 21:55:11 +0000

    Day 01 - variables, mutability, shadowing

--- block0/day01_variables/main.rs
+++ block0/day01_variables/main.rs
@@ -1,2 +1,17 @@
 // AnimaLab — day01_variables
-fn main() {}
+fn main() {
+    /*let monto = "1000";
+    monto = 2000;
+    println!("{}", monto);
+
+    let monto = monto + 500;
+    let monto = monto * 2;
+    println!("{}", monto);
+
+    let monto = monto.parse::<i32>().unwrap();
+    println!("{}", monto + 500);*/
+
+    let _estado = "pendiente";
+    let estado = 1;
+    println!("{}", estado);
+}
