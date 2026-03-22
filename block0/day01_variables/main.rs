439267d66e157a6500045f74d0d93492ee41b41f
Hendrik Ordoñez Bullon <hendrikobuw@gmail.com>
2026-03-22 13:08:09 +0000

    day01: variables exercise

--- block0/day01_variables/main.rs
+++ block0/day01_variables/main.rs
@@ -9,9 +9,21 @@ fn main() {
     println!("{}", monto);
 
     let monto = monto.parse::<i32>().unwrap();
-    println!("{}", monto + 500);*/
+    println!("{}", monto + 500);
 
     let _estado = "pendiente";
     let estado = 1;
     println!("{}", estado);
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("{}", x);
+
+    let mut x = "cinco";
+    x = x.len(&x);
+    println!("{}", x);*/
+    let nombre = "Paechaka";
+    let nombre = nombre.len();
+    println!("{}", nombre);
 }
